3ea3621a51351cb0a3674660717362dc16b7a5cb
VMurtazin <obdorsk@gmail.com>
2021-12-01 01:47:33 +0500

    delinitializer

--- src/main/java/ru/murtazin/springboottest/springboot_test/config/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package ru.murtazin.springboottest.springboot_test.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //пустой класс, использующийся для регистрации модуля в спринг-контейнере
-}
